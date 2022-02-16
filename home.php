08fdc074e04f1fcf32a5e5c6eed132532260af8d
nhuntington11 <nhuntington11@gmail.com>
2022-02-16 11:44:04 -0700

    styling and added images

--- home.php
+++ home.php
@@ -1,39 +1,78 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel='stylesheet' href='stylesheet.css'>
+	<title>Document</title>
+</head>
 
-?>
-
-<html>
-	<head>
-		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-		<link rel='stylesheet' href='stylesheet.css'>
-	</head>
-
-	<body id=page>
-		
-		<nav class='navbar navbar-default'>
-			<div class='container-fluid'>
+<body id='page'>
+	<div class='mt-4 p-5 bg-secondary text-body rounded'>	
+		<div class='container-fluid'>
+			<nav class='navbar navbar-default'>
 				<div class='navbar-header'>
 					<a class='navbar-brand logo' href='home.php'>Kitten Factory</a>
 				</div>
 				<ul class='nav navbar-nav'>
-					<li class='active'><a href='login.php'>Login</a></li>
+					<li class='active'><a href='login.php' class='text-body'>Login or Sign Up</a></li>
 				</ul>
+			</nav>
+		</div>
+	</div>
+
+	<div class='container-fluid'>
+		<div class="row p-5">
+			<div class="col-md-4">
+				<img src='images/atomic.png' class='img-fluid'>
+				<p class='mt-4'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis enim vel eleifend faucibus. Nullam sed lacus a ante imperdiet lacinia quis vel orci. Donec gravida nibh at turpis pretium, eget finibus lorem pharetra. Maecenas ac libero convallis, feugiat est eget, lacinia mauris. Vestibulum ac mauris congue, feugiat mauris ac, lobortis lorem. Vivamus quis dolor at massa dapibus accumsan id eu ex. Mauris sagittis volutpat orci id aliquet. Curabitur sollicitudin lectus odio, id porta ante finibus sed. Nam feugiat congue sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Fusce quis metus cursus, mollis magna vel, porta augue. Vivamus a aliquam mi. Sed eros elit, suscipit vel convallis eu, aliquam ut justo. Phasellus id dolor pretium, molestie nulla in, tincidunt metus. Ut placerat lorem vitae lacus volutpat, malesuada hendrerit erat pretium. 
+				</p>
 			</div>
-		</nav>
-			
-		<div class='container-fluid'>
-			<div class='row'>
-				<?php
-					$rows = 0;
-					while($rows <= 3) {
-						for($count=1; $count <=3; ++$count) {
-							echo "<div class='col'>".$count."</div>";
-						}
-						$count = 0;
-						++$rows;
-					}
-				?>
+			<div class="col-md-4">
+				<img src='images/dps.png' class='img-fluid'>
+				<p class='mt-4'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis enim vel eleifend faucibus. Nullam sed lacus a ante imperdiet lacinia quis vel orci. Donec gravida nibh at turpis pretium, eget finibus lorem pharetra. Maecenas ac libero convallis, feugiat est eget, lacinia mauris. Vestibulum ac mauris congue, feugiat mauris ac, lobortis lorem. Vivamus quis dolor at massa dapibus accumsan id eu ex. Mauris sagittis volutpat orci id aliquet. Curabitur sollicitudin lectus odio, id porta ante finibus sed. Nam feugiat congue sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Fusce quis metus cursus, mollis magna vel, porta augue. Vivamus a aliquam mi. Sed eros elit, suscipit vel convallis eu, aliquam ut justo. Phasellus id dolor pretium, molestie nulla in, tincidunt metus. Ut placerat lorem vitae lacus volutpat, malesuada hendrerit erat pretium. 
+				</p>
+			</div>
+			<div class="col-md-4">
+				<img src='images/kastle.png' class='img-fluid'>
+				<p class='mt-4'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis enim vel eleifend faucibus. Nullam sed lacus a ante imperdiet lacinia quis vel orci. Donec gravida nibh at turpis pretium, eget finibus lorem pharetra. Maecenas ac libero convallis, feugiat est eget, lacinia mauris. Vestibulum ac mauris congue, feugiat mauris ac, lobortis lorem. Vivamus quis dolor at massa dapibus accumsan id eu ex. Mauris sagittis volutpat orci id aliquet. Curabitur sollicitudin lectus odio, id porta ante finibus sed. Nam feugiat congue sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Fusce quis metus cursus, mollis magna vel, porta augue. Vivamus a aliquam mi. Sed eros elit, suscipit vel convallis eu, aliquam ut justo. Phasellus id dolor pretium, molestie nulla in, tincidunt metus. Ut placerat lorem vitae lacus volutpat, malesuada hendrerit erat pretium. 
+				</p>
 			</div>
 		</div>
-	</body>
+		<div class="row p-5">
+			<div class="col-md-4">
+				<img src='images/salomon.png' class='img-fluid'>
+				<p class='mt-4'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis enim vel eleifend faucibus. Nullam sed lacus a ante imperdiet lacinia quis vel orci. Donec gravida nibh at turpis pretium, eget finibus lorem pharetra. Maecenas ac libero convallis, feugiat est eget, lacinia mauris. Vestibulum ac mauris congue, feugiat mauris ac, lobortis lorem. Vivamus quis dolor at massa dapibus accumsan id eu ex. Mauris sagittis volutpat orci id aliquet. Curabitur sollicitudin lectus odio, id porta ante finibus sed. Nam feugiat congue sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Fusce quis metus cursus, mollis magna vel, porta augue. Vivamus a aliquam mi. Sed eros elit, suscipit vel convallis eu, aliquam ut justo. Phasellus id dolor pretium, molestie nulla in, tincidunt metus. Ut placerat lorem vitae lacus volutpat, malesuada hendrerit erat pretium. 
+				</p>
+			</div>
+			<div class="col-md-4">
+				<img src='images/volkl.png' class='img-fluid'>
+				<p class='mt-4'>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum venenatis enim vel eleifend faucibus. Nullam sed lacus a ante imperdiet lacinia quis vel orci. Donec gravida nibh at turpis pretium, eget finibus lorem pharetra. Maecenas ac libero convallis, feugiat est eget, lacinia mauris. Vestibulum ac mauris congue, feugiat mauris ac, lobortis lorem. Vivamus quis dolor at massa dapibus accumsan id eu ex. Mauris sagittis volutpat orci id aliquet. Curabitur sollicitudin lectus odio, id porta ante finibus sed. Nam feugiat congue sodales. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Fusce quis metus cursus, mollis magna vel, porta augue. Vivamus a aliquam mi. Sed eros elit, suscipit vel convallis eu, aliquam ut justo. Phasellus id dolor pretium, molestie nulla in, tincidunt metus. Ut placerat lorem vitae lacus volutpat, malesuada hendrerit erat pretium. 
+				</p>
+			</div>
+		</div>
+	</div>
+			
+	<div class='container-fluid'>
+		<div class='row'>
+			<?php
+				$rows = 0;
+				while($rows <= 3) {
+					for($count=1; $count <=3; ++$count) {
+						echo "<div class='col'>".$count."</div>";
+					}
+					$count = 0;
+					++$rows;
+				}
+			?>
+		</div>
+	</div>
+</body>
 </html>
--- login.php
+++ login.php
@@ -1,10 +1,40 @@
 <!DOCTYPE html>
-<html>
-	<head>
-		Login
-	</head>
-	
-	<body>
-		<p>TODO</p>
-	</body>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link rel='stylesheet' href='stylesheet.css'>
+	<title>Login</title>
+</head>
+<body>
+	<div class='mt-4 p-5 bg-secondary text-body rounded'>	
+		<div class='container-fluid'>
+			<nav class='navbar navbar-default'>
+				<div class='navbar-header'>
+					<a class='navbar-brand logo' href='home.php'>Kitten Factory</a>
+				</div>
+			</nav>
+		</div>
+	</div>
+	<div class='container w-25'>
+		<div class="row text-center">
+			<h1>Login</h1>
+		</div>
+		<form action="home.php" method='post'>
+			<div class="mb-3 mt-3">
+				<label for="email" class="form-label">Email</label>
+				<input type="email" class="form-control" placeholder="Email" name="email">
+			</div>
+			<div class="mb-3 mt-3">
+				<label for="password" class="form-label">Password</label>
+				<input type="password" class="form-control" placeholder="Password" name="password">
+			</div>
+			<div class="mb-3 mt-3">
+				<input type='submit' class="form-control">
+			</div>
+		</form>
+	</div>
+</body>
 </html>
